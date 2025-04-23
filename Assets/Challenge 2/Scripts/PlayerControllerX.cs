04e51cffced7dbbb9eb347130f911beab94def11
Novotny_Filip <novof107@hsd.k12.or.us>
2025-04-23 10:57:47 -0700

    Challenge 2 n.9 finished

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    private float cooldown = 1.0f;
+    private float cooldown = 0f;
     public GameObject dogPrefab;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown < Time.time)
         {
-            if (Time.deltaTime <= Time.deltaTime + cooldown)
-            {
-                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-            }
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = Time.time + 1.0f;
         }
     }
 }
